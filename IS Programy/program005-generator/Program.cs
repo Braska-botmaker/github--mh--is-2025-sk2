fb4eb5c003c6ee0d6c4ab3729fe0367b81d9cf93
Braska <horakmat@email.cz>
2025-11-06 13:01:31 +0100

    #8 základní kod

---
+++ IS Programy/program005-generator/Program.cs
@@ -0,0 +1,38 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("**************** Základní kod **************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Matěj Horák ******************");
+    Console.WriteLine("************** 6.11.2025 *******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    
+    
+    
+
+    
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+
+
+
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
